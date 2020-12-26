276c957185cc33fddbbce45078461befa8cafe0f
Ben Smus <bensmus@BENSMUSPC.localdomain>
2020-12-26 01:33:38 -0800

    Added on_guild_join event and change prefix option

--- mote.py
+++ mote.py
@@ -1,29 +1,46 @@
+# standard library
 import os
+import re
+
+# discord
+import discord
 from discord.ext import commands
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='!mote ')
+bot = commands.Bot(command_prefix='#')
 
 SHRUG = r'¯\_(ツ)_/¯'
 
 save_help_string = (
-    'intent: save text to your library of texts and give it an id and a label\n'
-    'usage: !mote save <id> <text> <label (optional)>\n'
-    f'example: !mote save shrug {SHRUG} emoticon'
+    'Save text to your library of texts and give it an id and a label\n'
 )
 
 id_help_string = (
-    'intent: display text saved in your library based on id\n'
-    'usage: !mote id <id>\n'
-    f'example: !mote id shrug --> displays {SHRUG}'
+    'Display text saved in your library based on id\n'
+)
+
+prefix_help_string = (
+    'Change the prefix used by the discord bot\n'
 )
 
 
-@bot.command(name='')
-async def just_prefix(ctx):
-    response = f'Hi, i\'m Mote! {SHRUG}\nType \"!mote help\" so I can tell you what I can do.'
-    return response
+@bot.event
+async def on_guild_join(guild):
+    response = (
+        'Joined :white_check_mark:\n'
+        '`-` My prefix is: `#`\n'
+        '`-` To change prefix: `#prefix <prefix>`\n'
+
+        '*Mote can be used to store plain text like hyperlinks '
+        'or emoticons!*\n'
+
+        'Type `#help` for more details.'
+    )
+
+    general = discord.utils.get(guild.channels, name='general')
+    await general.send(response)
 
 
 @bot.command(name='save', help=save_help_string)
@@ -35,6 +52,14 @@ async def save_text(ctx, id, text, label=None):
 @bot.command(name='id', help=id_help_string)
 async def get_text_by_id(ctx, id):
     response = 'id command detected'
+    await ctx.send(response)
+
+
+@bot.command(name='prefix', help=prefix_help_string)
+async def change_prefix(ctx, prefix):
+    old_prefix = bot.command_prefix
+    response = f'prefix changed from {old_prefix} to {prefix}'
+    bot.command_prefix = prefix
     await ctx.send(response)
 
 bot.run(TOKEN)
