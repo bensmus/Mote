0f8745fbe311c7eaa914037246b17121a7431e12
Ben Smus <bensmus@BENSMUSPC.localdomain>
2020-12-31 00:31:14 -0800

    Handling argument errors and improving formatting

--- mote.py
+++ mote.py
@@ -1,6 +1,6 @@
-# TODO: change the storage of ids associated with a user to a set
-# we want a sorted set becasuse we want newer ids to appear first
-# and also check to see if it is maximally clean
+# TODO: since your error_handle decorator returns a function that accepts
+# (*args, **kwargs) you have to make sure that the builtin help doesn't get
+# confused. Just try running help on any of the commands!
 
 # ----RUNNING LOCALLY----
 # https://realpython.com/python-redis/#using-redis-py-redis-in-python
@@ -39,6 +39,9 @@
 # a simpler command would be dump, and is a good starting point
 # dump shows all id belonging to person or channel (that have ever been defined)
 
+# identity preserver
+import functools
+
 # standard library
 import os
 
@@ -55,6 +58,16 @@ SHRUG = r'¯\_(ツ)_/¯'
 DEFAULT_PREFIX = '#'
 
 r = redis.from_url(REDIS_URL, decode_responses=True)
+
+
+def error_handle(func):
+    @functools.wraps(func)
+    async def handled(ctx, *args, **kwargs):
+        try:
+            await func(ctx, *args, **kwargs)
+        except Exception:
+            await ctx.send('Command argument error  :x:')
+    return handled
 
 
 async def determine_prefix(bot, message):
@@ -100,19 +113,21 @@ async def on_guild_join(guild):
 
 
 save_help_string = (
-    'Save text with ID and label. This command is context dependent.\n'
+    'Save text with ID and label.\n'
     '`-` if you invoke (type it) the command in a DM, it will save to your '
-    'personal library\n'
+    'personal library.\n'
     '`-` if you invoke it in a server text channel, '
     'it will save to the channel library.'
 )
 
-id_help_string = 'Display entries based on ID.'
-dump_help_string = 'Displays all your entries'
+get_help_string = 'Display entries based on ID.'
+delete_help_string = 'Deletes entries based on ID.'
+dump_help_string = 'Displays all your entries.'
 prefix_help_string = 'Change the prefix used by the discord bot.'
 
 
 @bot.command(name='save', help=save_help_string)
+@error_handle
 async def save_text(ctx, text_id, text):
 
     # detect DM
@@ -127,12 +142,12 @@ async def save_text(ctx, text_id, text):
     response = (
         f'ID: `{text_id}`\n'
         f'Text: `{text}`\n'
-        f'Saved to `{savetype}` {emoji} library :white_check_mark:'
+        f'Saved to `{savetype}` {emoji} library  :white_check_mark:'
     )
 
-    # redis store (text_id, text) @ text_library
+    # redis store (text_id, text) @ library
     key = str(savetype.id) + ':' + text_id
-    r.hset('text_library', key, text)
+    r.hset('library', key, text)
 
     # redis store text_ids that belong to user or channel
     # using sorted set as unique value list
@@ -142,7 +157,8 @@ async def save_text(ctx, text_id, text):
     await ctx.send(response)
 
 
-@bot.command(name='id', help=id_help_string)
+@bot.command(name='get', help=get_help_string)
+@error_handle
 async def get_text_by_id(ctx, text_id):
 
     # detect DM
@@ -152,16 +168,39 @@ async def get_text_by_id(ctx, text_id):
     else:
         key = str(ctx.channel.id) + ':' + text_id
 
-    value = r.hget('text_library', key)
+    value = r.hget('library', key)
 
     if value == None:
-        await ctx.send('No text found based on the given id :cry:')
+        await ctx.send('Nonexistent ID  :robot:')
 
     else:
         await ctx.send(value)
 
 
+@bot.command(name='delete', help=delete_help_string)
+@error_handle
+async def delete_text_by_id(ctx, *text_ids):
+
+    # detect DM
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        delete_as = ctx.author.id
+
+    else:
+        delete_as = ctx.channel.id
+
+    # 1) remove the id from the id's that are associated with author/channel
+    r.zrem(delete_as, *text_ids)  # unpacking argument list
+
+    # 2) remove the id and text from the library
+    # get a tuple of delete_as + : + text_id
+    keys = tuple(map(lambda text_id: str(delete_as) + ':' + text_id, text_ids))
+    r.hdel('library', *keys)
+
+    await ctx.send('Key(s) deleted  :white_check_mark:')
+
+
 @bot.command(name='dump', help=dump_help_string)
+@error_handle
 async def dump_text(ctx):
 
     # the channel or author that we are dumping as
@@ -177,29 +216,34 @@ async def dump_text(ctx):
     # getting total number of elements in sorted set regardless of score
     number_saved = r.zcount(dump_as, '-inf', 'inf')
 
-    # print(f'number_saved = {number_saved}')
-    # print(r.lrange(dump_as, 0, number_saved))
+    if number_saved != 0:
+        # print(f'number_saved = {number_saved}')
+        # print(r.lrange(dump_as, 0, number_saved))
 
-    dump_ids = r.zrange(dump_as, 0, number_saved)
-    dump_string = ''
+        dump_ids = r.zrange(dump_as, 0, number_saved)
+        dump_string = ''
 
-    for text_id in dump_ids:
-        dump_string += (text_id + '\n')
+        for text_id in dump_ids:
+            dump_string += (text_id + '\n')
 
-    await ctx.send(dump_string)
+        await ctx.send(dump_string)
+
+    else:
+        await ctx.send('No ID\'s found to dump  :robot:')
 
 
 def prefix_check(prefix, old_prefix):
     if len(prefix) == 1:
         if not (prefix.isdigit() or prefix.isalpha()):
             if prefix != '@':
-                return True, f'Prefix changed from {old_prefix} to {prefix} :white_check_mark:'
-            return False, 'Prefix cannot be changed to @ :x:'
-        return False, 'Prefix cannot be number or letter :x:'
-    return False, 'Prefix cannot be more than one character long :x:'
+                return True, f'Prefix changed from {old_prefix} to {prefix}  :white_check_mark:'
+            return False, 'Prefix cannot be changed to @  :x:'
+        return False, 'Prefix cannot be number or letter  :x:'
+    return False, 'Prefix cannot be more than one character long  :x:'
 
 
 @ bot.command(name='prefix', help=prefix_help_string)
+@error_handle
 async def change_prefix(ctx, prefix):
     guild = ctx.guild
     if guild:
@@ -213,7 +257,7 @@ async def change_prefix(ctx, prefix):
         await ctx.send(response)
 
     else:
-        await ctx.send('Prefix command cannot be used in DM channels :x:')
+        await ctx.send('Prefix command cannot be used in DM channels  :x:')
 
 
 bot.run(TOKEN)
--- storage_model.md
+++ storage_model.md
@@ -1,10 +1,11 @@
 **Storage model**
 For prefixes: 
-- prefix hash (guild: prefix)
+- prefix hash {guild: prefix}
 
 For library:
-- library hash (channel/author.id+id: value) 
-- list of id's corresponding to author.id/channel.id 
+- library hash {channel/author.id+id: value}
+- sorted set of id's corresponding to author.id/channel.id 
+    - sorted set so that adding an id is handled automatically (we don't have to think about duplicates)
 
 **Unique id's provided by Discord API**
 ctx.author.id <--- unique (for storing DM library)
@@ -19,30 +20,14 @@ redis>
 
 **Prefix changing is the only thing that should be done server wide, don't allow prefix changing for DM's**
 
-**For DM's:** 
-- have a list, which makes `#label league` possible
-    author0.id = [author0.id:id0, author0.id:id1, ... author0.id:idN]
-- then also we have a hash table 
-    table = {author:id0:'text0 label0', author4:id0:'text5 label5'}
-
 *Discord command and corresponding redis call: in a DM*
 
 discord> #save lux-support https://www.mobafire.com/mobafire.com/lux-support/best-player-NA league
 
-redis> HSET myhash author.id:lux-support https://www.mobafire.com/mobafire.com/lux-support/best-player-NA league
+redis> HSET myhash author.id:lux-support https://www.mobafire.com/mobafire.com/lux-support/best-player-NA 
 
 discord> #id lux-support
 
 redis> HGET author.id:lux-support 
 https://www.mobafire.com/mobafire.com/lux-support/best-player-NA league
 
-*Discord command and corresponding redis call: in a channel*
-
-discord> #save lux-support https://www.mobafire.com/mobafire.com/lux-support/best-player-NA league
-
-redis> HSET myhash channel.id:lux-support https://www.mobafire.com/mobafire.com/lux-support/best-player-NA league
-
-**Python command to get first word in a sentance**
-python> s = 'Hello World'
-python> s.split()[0]
-'Hello'
